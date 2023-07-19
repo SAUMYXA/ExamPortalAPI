64a0869868b3469e27e7926210508d0fb5ab9eba
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2023-07-19 22:50:35 +0530

    feat:postCandidate has been added. this is done by the admin side to enter the details of the candidate

---
+++ controllers/user.js
@@ -0,0 +1,41 @@
+const asyncHandler = require("express-async-handler");
+const user=require('../models/user')
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {
+        name,
+        branch,
+        section,
+        studentNo,
+        registrationNo,
+        phoneNo,
+        email,
+        gender,
+        hostler,
+      } = req.body;
+    const userAvailable = await user.findOne({ email });
+    if (userAvailable) {
+      res
+        .status(400)
+        .json({ msg: "already registered." });
+    }
+   
+ 
+  if (!registrationNo || !studentNo) {
+    res.status(400).json({ msg: "fill all the credentials!" });
+  }
+
+ let createUser = await user.create({
+    name,
+    branch,
+    section,
+    studentNo,
+    registrationNo,
+    phoneNo,
+    email,
+    gender,
+    hostler,
+  });
+  res.status(201).json({msg:"User has been successfully added.",createUser})
+});
+module.exports=registerUser;
--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,5 @@
 const express=require('express');
+const registerUser = require('../controllers/user');
 const router=express.Router();
+router.post('/addcandidate',registerUser);
+module.exports=router;
